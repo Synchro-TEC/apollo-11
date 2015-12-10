0a519df8077af87c0bf157c8e5e8939fe2252290
Marcio Gasparotto <mgasparotto@gmail.com>
2015-12-09 23:56:19 -0200

    components skeleton

---
+++ src/hermes/hermes.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+class Hermes extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>Hermes</h1>
+      </div>
+    );
+  }
+}
+
+Hermes.propTypes = {};
+
+Hermes.displayName = 'Hermes';
+
+export default Hermes;
---
+++ src/launch-window/launch-window.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+class LaunchWindow extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>Launch Window</h1>
+      </div>
+    );
+  }
+}
+
+LaunchWindow.propTypes = {};
+
+LaunchWindow.displayName = 'LaunchWindow';
+
+export default LaunchWindow;
