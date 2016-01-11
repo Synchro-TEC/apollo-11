d2f98cf02e8e2ff1c03d5b4b22af1688ff3120aa
Marcio Gasparotto <mgasparotto@gmail.com>
2016-01-11 18:24:07 -0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Hermes from './hermes/hermes';
 import LaunchWindow from './launch-window/launch-window';
 import Blocker from './blocker/blocker';
+import OptionSelector from './opition-selector/option-selector'
 
 export {
   Hermes,
   LaunchWindow,
-  Blocker
+  Blocker,
+  OptionSelector
 };
---
+++ src/opition-selector/option-selector.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+
+const optionSelectorStyles = {
+  container: {
+    position: 'relative'
+  },
+  options: {
+    padding: '15px',
+    backgroundColor: '#fff',
+    boxShadow: '0 0 5px #d1d1d1',
+    display: 'none',
+    position: 'absolute',
+    listStyle: 'none',
+    zIndex: '99'
+  }
+}
+
+class OptionSelector extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {label: this.props.initialValue, isVisible: false};
+  }
+
+  onSelect(value) {
+    this.setState({label: value, isVisible: false});
+    this.props.onSelectCallBack(value);
+  }
+
+  showOptions() {
+    this.setState({label: this.state.label, isVisible: !this.state.isVisible});
+  }
+
+  render() {
+    let options = this.props.options.map((opt, i) => {
+      return <li key={`opt-${i}`} onClick={() => this.onSelect(opt.value)} style={{cursor: 'pointer'}}>{opt.label}</li>;
+    });
+
+    var display = this.state.isVisible ? {display: 'block'} : {display: 'none'};
+    var optsStyles = Object.assign({}, optionSelectorStyles.options, display);
+
+    return (
+      <div style={optionSelectorStyles.container}>
+        <button className="sv-button small default" onClick={()=>this.showOptions()}>{this.state.label}</button>
+        <ul style={optsStyles}>
+          {options}
+        </ul>
+      </div>
+    )
+  }
+}
+
+OptionSelector.displayName = 'OptionSelector';
+
+OptionSelector.propTypes = {
+  initialValue: React.PropTypes.string,
+  onSelectCallBack: React.PropTypes.func,
+  options: React.PropTypes.array
+};
+
+OptionSelector.defaultProps = {
+  initialValue: '?'
+}
+
+export default OptionSelector;
