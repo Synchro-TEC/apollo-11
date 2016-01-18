5d14be936227373638534e0be386dff86d06e31f
Marcio Gasparotto <mgasparotto@gmail.com>
2016-01-18 08:56:23 -0200

    wip

--- src/opition-selector/option-selector.js
+++ src/opition-selector/option-selector.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
-
 const optionSelectorStyles = {
   container: {
     display: 'inline',
     position: 'relative'
   },
   options: {
-    padding: '15px',
+    padding: '10px',
     backgroundColor: '#fff',
     boxShadow: '0 0 5px #d1d1d1',
     display: 'none',
     position: 'absolute',
     listStyle: 'none',
     zIndex: '99'
+  },
+  li: {
+    display: 'block',
+    cursor: 'pointer',
+    margin: '10px 0',
+    lineHeight: '1.5',
   }
 }
 
@@ -36,15 +41,14 @@ class OptionSelector extends React.Component {
 
   render() {
     let options = this.props.options.map((opt, i) => {
-      return <li key={`opt-${i}`} onClick={() => this.onSelect(opt)} style={{cursor: 'pointer'}}>{opt[this.props.labelKey]}</li>;
+      return <li key={`opt-${i}`} onClick={() => this.onSelect(opt)} style={optionSelectorStyles.li}>{opt[this.props.labelKey]}</li>;
     });
-
     var display = this.state.isVisible ? {display: 'block'} : {display: 'none'};
     var optsStyles = Object.assign({}, optionSelectorStyles.options, display);
 
     return (
       <div style={optionSelectorStyles.container}>
-        <button className="sv-button small default" onClick={()=>this.showOptions()}>{this.state.label}</button>
+        <button className="sv-button link link-info" onClick={()=>this.showOptions()}>{this.state.label}</button>
         <ul style={optsStyles}>
           {options}
         </ul>
