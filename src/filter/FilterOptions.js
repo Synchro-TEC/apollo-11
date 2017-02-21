9494eecb2764c4574be3de55a697d87792b7527b
Gabriel Naressi <gnf@ca208.synchro.com.br>
2017-02-21 14:00:47 -0300

    Detalhe no filtro para ser possível a filtragem por campos de texto e refactor

--- src/filter/FilterOptions.js
+++ src/filter/FilterOptions.js
@@ -17,6 +17,7 @@ class FilterOptions extends React.Component {
   }
 
   componentDidMount() {
+    debugger;
     for(let i = 0; i<this.namesOfFields.length; i++) {
       // Nomes iguais, nao podem ser colocados no formFields
       if(this.namesOfFields[i] !== this.namesOfFields[i+1]) {
@@ -43,19 +44,17 @@ class FilterOptions extends React.Component {
 
   applyFilter() {
     this.fields.map((field) => {
-      if(field.type === 'select-one' && field.value !== '') {
-        this.filterValues[field.name] = field.value;
-      } else {
-        if(field.type === 'radio' && field.checked) {
+      if(field.type === 'text' || field.type === 'select-one') {
+        if(field.value !== '') {
           this.filterValues[field.name] = field.value;
+        }
+      } else if(field.type === 'radio' && field.checked) {
+        this.filterValues[field.name] = field.value;
+      } else if(field.type === 'checkbox' && field.checked) {
+        if(this.filterValues[field.name]) {
+          this.filterValues[field.name].push(field.value);
         } else {
-          if (field.type === 'checkbox' && field.checked) {
-            if(this.filterValues[field.name]) {
-              this.filterValues[field.name].push(field.value);
-            } else {
-              this.filterValues[field.name] = [field.value];
-            }
-          }
+          this.filterValues[field.name] = [field.value];
         }
       }
     });
@@ -71,29 +70,31 @@ class FilterOptions extends React.Component {
     this.setState({isOpen: false});
   }
 
-  renderChildrenAndGetFieldNames(children) {
+  verifyTypesAndGetNames(child) {
+    if(child.type === 'input') {
+      if(child.props.type === 'text' ||
+         child.props.type === 'radio' ||
+         child.props.type === 'checkbox') {
+        this.namesOfFields.push(child.props.name);
+      }
+    } else if(child.type === 'select') {
+      this.namesOfFields.push(child.props.name);
+    }
+  }
+
+  renderChildren(children) {
     return (
       React.Children.map(children, (child) => {
         if(Array.isArray(children)) {
-          if(child.type === 'select') {
-            this.namesOfFields.push(child.props.name);
-          }
+          this.verifyTypesAndGetNames(child);
           if(typeof child !== 'string') {
-            if(child.props.type === 'checkbox') {
-              this.namesOfFields.push(child.props.name);
-            }
-            if(child.props.type === 'radio') {
-              this.namesOfFields.push(child.props.name);
-            }
-            this.renderChildrenAndGetFieldNames(child.props.children);
+            this.renderChildren(child.props.children);
           }
           return child;
         } else {
           if(!_isEmpty(children.props)) {
-            if(children.props.type === 'radio') {
-              this.namesOfFields(children.props.name);
-            }
-            this.renderChildrenAndGetFieldNames(child.props.children);
+            this.verifyTypesAndGetNames(child);
+            this.renderChildren(child.props.children);
             return child;
           }
         }
@@ -132,7 +133,7 @@ class FilterOptions extends React.Component {
         <section className='action-container' style={filterOptionsStyles}>
           <div className='sv-triangle on-right'/>
           <section className='action-container--content'>
-            {this.renderChildrenAndGetFieldNames(this.props.children)}
+            {this.renderChildren(this.props.children)}
             <footer>
               <button
                 className='sv-button link link-danger sv-pull-left'
